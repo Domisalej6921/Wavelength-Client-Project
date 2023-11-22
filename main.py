5c2eef09a11e9d2c9390532421ee277ed83d6e23
Dominick George <georged8@cardiff.ac.uk>
2023-11-22 10:05:16 +0000

    Created main page framework, very initial stages and needs to be further implemented, done some css and flask

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 
 app.secret_key = 'isodhfudyf8d9asye*sufhsd789fhds89ufhysuifdhsgf78fhasoidhsaidlgho;dhsagofudgfdiosufga9s8dfg7f8s'
 
-# Route for the index page
+# Route for the index page ----- delete after --- commented index out so that I could view the main page
+#@app.route('/')
+#def index():
+    #return render_template('index.html')
+
+#Route for main page
 @app.route('/')
-def index():
-    return render_template('index.html')
+def main_page():
+    return render_template('main_page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

84efa54297f3795e7ee35b893a6c97c5b33d53f5
Thomas Brown <brownt17@cardiff.ac.uk>
2023-11-22 09:26:51 +0000

    appsettings.json

--- data/AccountRepository.py
+++ data/AccountRepository.py
@@ -1,5 +1,20 @@
 from data.DataHelper import DataHelper
 
 class AccountRepository:
-    def __init__(self):
-        self.db = DataHelper()
+    # Inherit the DataHelper class
+    db = DataHelper()
+
+    @staticmethod
+    def GetWithEmail(self, email: str):
+        """Gets an account with an email."""
+        return self.db.SelectFirstWithParams("SELECT * FROM Accounts WHERE Email = ?", (email,))
+
+    @staticmethod
+    def GetWithUsername(self, username: str):
+        """Gets an account with a username."""
+        return self.db.SelectFirstWithParams("SELECT * FROM Accounts WHERE Username = ?", (username,))
+
+    @staticmethod
+    def Insert(self):
+        """Inserts an account into the database."""
+        pass

8174892df4ffab7a5ceac014f0e3393c84117161
Thomas Brown <brownt17@cardiff.ac.uk>
2023-11-23 01:12:46 +0000

    Improved dbImport.py

--- dbImport.py
+++ dbImport.py
@@ -1,4 +1,14 @@
+import os
 from data.DataHelper import DataHelper
+
+# Check the databases directory exists and if not create it
+if not os.path.exists("data/databases"):
+    os.makedirs("data/databases")
+
+# Check if the general.db file exists and if not create it
+if not os.path.exists("data/databases/general.db"):
+    file = open("data/databases/general.db", "w")
+    file.close()
 
 # Create DB instance
 db = DataHelper()

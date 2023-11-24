506425649eb4fd7aea0d64d01c46ceaaaa922397
Harley Doe <doeh@cardiff.ac.uk>
2023-11-24 15:08:54 +0000

    fixed_camel_casing and split model into two files and renamed functions

--- models/headerModel.py
+++ models/headerModel.py
@@ -1,52 +1,7 @@
-class FooterModel:
-
-    @staticmethod
-    def basic_footer() -> str:
-        return f"""
-        <div class="fixed-bottom bg-primary" style="--bs-bg-opacity: .175;">
-
-            <div class="container text-center">
-              <div class="row align-items-center">
-                <div class="col justify-items-start">
-                  <a class="footer-logo" href="/home">
-                      <img class="logo" src="../static/images/logo-temp.png"  alt="" width="45" height="45">
-                  </a>
-                </div>
-                <div class="col">
-                    <p class="copyright-text"> © 2023 Wavelength. All rights reserved. </p>
-                </div>
-                <div class="col">
-                    <h5 class="title-contacts-text">Contact Us:</h5>
-                  <ul class="contacts-list">
-                      <li class="contacts-text">
-                          Email:
-                          <a class="contacts-links" href="example@example.co.uk">
-                              example@example.co.uk
-                          </a>
-                      </li>
-                      <li class="contacts-text">
-                          Phone:
-                          <a class="contacts-links" href="0777777777">
-                              0777777777
-                          </a>
-                      </li>
-                      <li class="contacts-text">
-                          Address:
-                          <a class="contacts-links" href="Example Address">
-                              Example Address
-                          </a>
-                      </li>
-                    </ul>
-                </div>
-              </div>
-            </div>
-
-        </div>"""
-
 class HeaderModel:
 
     @staticmethod
-    def basic_header() -> str:
+    def standardHeader() -> str:
         return f"""
         <nav class="navbar bg-primary" style="--bs-bg-opacity: .175;">
 
@@ -86,7 +41,7 @@ class HeaderModel:
         </nav>"""
 
     @staticmethod
-    def logged_header() -> str:
+    def loggedInHeader() -> str:
         return f"""
         <nav class="navbar bg-primary" style="--bs-bg-opacity: .175;">
 

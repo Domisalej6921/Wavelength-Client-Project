5e6b2bbb06156719f87d65bcb68073348d03abfb
Harley Doe <doeh@cardiff.ac.uk>
2023-11-28 10:39:40 +0000

    Added basic js to collect the values in the form

--- static/javascript/moderator/credit-generation.js
+++ static/javascript/moderator/credit-generation.js
@@ -1,5 +1,13 @@
 class CreditGeneration {
     static submitForm() {
 
+        const data = {
+            username: document.getElementById("numCredits").value,
+            name: document.getElementById("numGroups").value,
+            email: document.getElementById("chooseCommunity").value,
+        }
+
+        
+
     }
 }

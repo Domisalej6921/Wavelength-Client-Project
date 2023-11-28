2af68eef3217372c9c2f2e3daa445a1b4e02afbd
Harley Doe <doeh@cardiff.ac.uk>
2023-11-28 11:40:20 +0000

    created more variables in JS that allow me to hide the other elements that would not be needed when the user wants a new community

--- static/javascript/moderator/credit-generation.js
+++ static/javascript/moderator/credit-generation.js
@@ -1,18 +1,22 @@
 class CreditGeneration {
     static submitForm() {
 
-        document.addEventListener(type: "DOMContentLoaded", listener:function() {
+        document.addEventListener(type: "DOMContentLoaded", listener: function() {
             var unlock = document.getElementById(elementId: "existingCommunity");
             var chooseCommunityDiv = document.getElementById(elementId:"chooseCommunityDiv");
             var createCommunityDiv = document.getElementById(elementId:"createCommunityDiv");
+            var numCreditsDiv = document.getElementById(elementId:"numCreditsDiv");
+            var numGroupsDiv = document.getElementById(elementId:"numGroupsDiv");
 
-            unlock.addEventListener(type:"change", listener:function() {
+            unlock.addEventListener(type:"change", listener: function() {
                 if (unlock.checked) {
                     chooseCommunityDiv.style.display = "block";
                     createCommunityDiv.style.display = "None";
 
                 } else {
                     chooseCommunityDiv.style.display = "None";
+                    numCreditsDiv.style.display = "None";
+                    numGroupsDiv.style.display = "None";
                     createCommunityDiv.style.display = "block";
                 }
             })

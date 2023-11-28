47ad14210c7754893b264be9dd7ac8651de9174c
Harley Doe <doeh@cardiff.ac.uk>
2023-11-28 11:13:03 +0000

    added temporary code that checks for event from checkbox to hide/show other info

--- static/javascript/moderator/credit-generation.js
+++ static/javascript/moderator/credit-generation.js
@@ -1,13 +1,25 @@
 class CreditGeneration {
     static submitForm() {
 
+        document.addEventListener(type: "DOMContentLoaded", listener:function() {
+            var unlock = document.getElementById(elementId: "existingCommunity");
+            var optionsDiv = document.getElementById(elementId:"optionsDiv");
+
+            unlock.addEventListener(type:"change", listener:function() {
+                if (unlock.checked) {
+                    optionsDiv.style.display = "block";
+                } else {
+                    optionsDiv.style.display = "None";
+                }
+            })
+        })
+
         const data = {
             username: document.getElementById("numCredits").value,
             name: document.getElementById("numGroups").value,
             email: document.getElementById("chooseCommunity").value,
         }
 
-        
 
     }
 }

9b65c423419b52a8d1fcda3490dd4542307beb7b
Harley Doe <doeh@cardiff.ac.uk>
2023-12-01 10:11:16 +0000

    added a lot of functionality in the python files to interact with the databases

--- blueprints/creditGeneration.py
+++ blueprints/creditGeneration.py
@@ -6,6 +6,31 @@ from logic.cryptography import Cryptography
 
 creditGeneration = Blueprint("creditGeneration", __name__)
 
+def generateTokenId()
+    return
+def getCommunity(self, name: str, responseId: bool):
+
+    if responseId == True:
+        community_Id = "SELECT EntityId FROM Entities WHERE Name IS name"
+        return community_Id
+
+    elif responseId == False:
+        community_name = "SELECT Name FROM Entities WHERE Name IS name"
+        return community_name
+
+def getCommunitiesSimilar(self, searchTerm: str):
+    possibleCommunities = []
+    possibleCommunities.append("SELECT * FROM Entities WHERE Name LIKE searchTerm")
+    return possibleCommunities
+
+def createTokens(self, newTokenId: int, ownerID: int, ownerType: int, timeCreated: int):
+
+    ("INSERT INTO Tokens (TokenID, OwnerID, OwnerType, Created)"
+     "VALUES ('newTokenId', 'ownerID', 'ownerType', 'timeCreated')")
+
+    tokenId = newTokenId
+    return tokenId
+
 @creditGeneration.route("/listCommunities", methods=["POST"])
 def listCommunities():
     communities = []
@@ -31,8 +56,22 @@ def search():
 # );
 
 @creditGeneration.route("/create", methods=["POST"])
-def create():
+def create(data: object):
     creditsMade = []
+
+    tokenIds = []
+
+    while numTokens > 0:
+        newTokenId = generateTokenId()
+        tokenIds.append(newTokenId)
+        communityId = getCommunity(chosenCommunity)
+        tokenIds.append(createTokens(newTokenId, communityId))
+        numTokens = - 1
+
+
+    # get the community id
+    # generate the token id s
+    # assign the token id with the community id and the ownerType(community) add created time
 
     returnedValues = "temporary text"
 

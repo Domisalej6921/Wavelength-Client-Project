b9614bbf284e7f78b1479e5d8bbe145e2fdc0bef
Harley Doe <doeh@cardiff.ac.uk>
2023-12-04 15:28:05 +0000

    added the functions into the js

---
+++ static/javascript/moderator/delete-credits.js
@@ -0,0 +1,22 @@
+class CreditDeletion {
+    //set up the class for the js to be called from
+
+    static onLoadFunctions() { //Function that calls the functions to run when the page loads
+        this.getInactiveCredits()
+    }
+
+    //Function that calls the database to get all credits that haven't been used in over 1 month or more
+    static getInactiveCredits() {
+
+    }
+
+    //Function which sorts the inactive credits by the time the mod wants
+    static sortInactiveCredits() {
+
+    }
+
+    //Function to delete the credits
+    static deleteCredits() {
+
+    }
+}

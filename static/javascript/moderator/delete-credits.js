b8647e25e35261f8889b08bfd2ec3a1a748d6148
Harley Doe <doeh@cardiff.ac.uk>
2023-12-04 17:43:55 +0000

    setup the basis for creating a graph to display inactive credits

--- static/javascript/moderator/delete-credits.js
+++ static/javascript/moderator/delete-credits.js
@@ -3,16 +3,33 @@ class CreditDeletion {
 
     static onLoadFunctions() { //Function that calls the functions to run when the page loads
         this.getInactiveCredits()
+        this.totalCredits()
     }
 
     //Function that calls the database to get all credits that haven't been used in over 1 month or more
     static getInactiveCredits() {
+
+        const xhttp = new XMLHttpRequest(); // creates new XMLHttp request
+            xhttp.open("POST", "/getInactiveCredits", true); //set method and the url and if it asynchornus
+            xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) { //200 = server is okay
+                    const response = JSON.parse(this.responseText); // passing back the server response
+                    console.log(response.result);
+                }
+            };
+
+    }
+
+    static totalCredits () { //Function to count the total credits being deleted
 
     }
 
     //Function which sorts the inactive credits by the time the mod wants
     static sortInactiveCredits() {
 
+        this.totalCredits()
     }
 
     //Function to delete the credits

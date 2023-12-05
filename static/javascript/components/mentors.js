ad4561fb404e0a1a8b04560966972f2bfca49fc0
Dominick George <georged8@cardiff.ac.uk>
2023-12-05 14:04:41 +0000

    Finished back-end of rendering images to webpage and testing.

--- static/javascript/components/mentors.js
+++ static/javascript/components/mentors.js
@@ -2,7 +2,7 @@ class Mentors {
     static renderCard(data) {
         return `
             <div id="mentor#${data.userID}" class="card" style="width: 18rem;">
-                <img id="image" class="card-img-top" alt="${data.description}">
+                <img src="/static/uploads/${data.profilePicture.path}" class="card-img-top" alt="${data.profilePicture.alt}">
                 <div class="card-body">
                     <h5 class="card-title">${data.name}</h5>
                     <p class="card-text">${data.description}</p>
--- static/javascript/mainPage.js
+++ static/javascript/mainPage.js
@@ -1,9 +1,23 @@
 class MainPage {
-    static async renderMentors(){
-        const response = await fetchMentors.getWithLimit()
+    static async renderMentors(limit = 12){
+        const response = await fetchMentors.getWithLimit(limit)
 
         if (response.status === 200) {
             console.log("FUCK OFF AKSHAY")
+
+            // Get data from the response
+            let data = await response.json()
+
+            console.log(data[0])
+
+            // Render mentors onto the page
+            let mentors = "";
+            for (let i = 0; i < data.length; i++) {
+                mentors += Mentors.renderCard(data[i])
+            }
+
+            // Render it onto the page
+            document.getElementById('mentorList').innerHTML = mentors;
         }
         else if (response.status === 401) {
             window.location.href = "/login"

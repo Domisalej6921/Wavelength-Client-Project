6644631e9ffaacac462716c1aecd700e35af8042
C23069362 <georged8@cardiff.ac.uk>
2023-12-07 21:23:15 -0800

    Finally fixed the js not importing into the html file, this caused the onclick attribute to not work correctly

--- static/javascript/mainPage.js
+++ static/javascript/mainPage.js
@@ -27,3 +27,28 @@ class MainPage {
         }
     }
 }
+
+let originalContainer = document.getElementById("mentorList");
+let loadMoreBtn = document.getElementById("LoadMoreButton");
+let itemsPerPage = 3;
+let limit = 12;
+let totalItems = originalContainer.children.length;
+let currentItems = itemsPerPage;
+
+
+function loadMoreButton() {
+        loadMoreBtn.addEventListener("click", function () {
+        let newItemsContainer = document.createElement("div");
+        newItemsContainer.className = "card";
+        newItemsContainer.textContent = "working";
+            // Mentors.renderCard(data);
+
+        limit += 3
+
+        // MainPage.renderMentors(limit)
+
+        originalContainer.appendChild(newItemsContainer);
+        currentItems += itemsPerPage;
+
+    })
+}

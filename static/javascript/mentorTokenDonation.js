0c6142d670150b3e61f878a1423edfebc9377d8f
akshay karsan <karsana@cardiff.ac.uk>
2023-12-12 09:45:15 +0000

    Added the HTML for the page where mentors can donate tokens to a chosen community, now i need to work on the javascript and the python backend

---
+++ static/javascript/mentorTokenDonation.js
@@ -0,0 +1,8 @@
+class CreateCommunity {
+    static async submitForm() {
+        // Set the form button to a loading state
+        document.getElementById("mentorTokenDonation").innerHTML = Buttons.getPastelButtonLoading('Creating...', 'lg');
+
+
+    }
+}
